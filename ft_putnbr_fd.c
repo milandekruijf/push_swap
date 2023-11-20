998e20d8b75d4b5740c4a0d4770eea4789f3a7ca
Milan De Kruijf <mde-krui@f0r1s4.codam.nl>
2023-11-20 11:15:30 +0100

    used ft_itoa for putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mde-krui <mde-krui@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/02 17:36:17 by mde-krui      #+#    #+#                 */
-/*   Updated: 2023/10/09 15:41:58 by mde-krui      ########   odam.nl         */
+/*   Updated: 2023/11/20 11:15:18 by mde-krui      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,5 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-		return (ft_putstr_fd("-2147483648", fd));
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd((n % 10) + '0', fd);
+	ft_putstr_fd(ft_itoa(n), fd);
 }
